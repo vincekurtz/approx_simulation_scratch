1f1b7df26a84901ff8264ef49a2e9e14742589a1
Vince Kurtz <vjkurtz@gmail.com>
2019-03-18 10:54:45 -0400

    add epsilon

--- input_state_stability.m
+++ input_state_stability.m
@@ -1,6 +1,7 @@
 % We can formulate the simulation problem as Input-Output stability of the
 % joint system, so here we'll consider starting from an ISS perspective
 
+close all;
 clear all;
 clc;
 
@@ -26,6 +27,8 @@ P = [1;0];
 x1 = sdpvar(1);
 x2 = sdpvar(1);
 x3 = sdpvar(1);
+x = [x1;x2;x3];
+x0 = [0.2;0;0];   % initial conditions
 
 u = sdpvar(1);
 
@@ -34,22 +37,26 @@ f = [A1*[x1;x2] + B1*[u+K*([x1;x2]-P*x3)];
      A2*x3 + B2*u];
  
 % System outputs
-g1 = x1;
-g2 = x3;
 g = x1-x3;
 
 % Candidate ISS Lyapunov function
-[V,vc] = polynomial([x1;x2;x3],3);
+[V,vc] = polynomial([x1;x2;x3],2);
 Vdot = jacobian(V,[x1;x2;x3])*f;
+
+% We'll try to minimize this
+V0 = replace(V,x,x0);
+
+% For ensuring PD-ness
+epsilon = 1e-8;
 
 % Class K/K_inf functions that should bound Vdot
 [alpha,ac] = polynomial([x1;x2;x3],3);
-[gamma,gc] = polynomial([u],4);
-[alpha_,a_c] = polynomial([g],3);
+[gamma,gc] = polynomial([u],3);
+[alpha_,a_c] = polynomial([g],2);
 
 % SOS constraints
-F = [sos(V-alpha_)];
-F = F + [sos(alpha),sos(gamma)];
+F = [sos(V-alpha_-epsilon*x'*x)];
+F = F + [sos(alpha),sos(gamma),sos(alpha_)];
 F = F + [sos(-alpha+gamma-Vdot)];
 
 [sol,u,Q,res] = solvesos(F,[],[],[vc;ac;gc;a_c]);
@@ -69,13 +76,13 @@ dt = 0.1;
 Ns = 500;
 
 % Place to record the results
-x1 = zeros(2,Ns);
-x2 = zeros(1,Ns);
+x1_sim = zeros(2,Ns);
+x2_sim = zeros(1,Ns);
 u2 = zeros(1,Ns);
 
 % Initial conditions
-x1(:,1) = [0.2;0];
-x2(:,1) = 0;
+x1_sim(:,1) = x0(1:2);
+x2_sim(:,1) = x0(3);
 for i = 1:(Ns-1)
     u2(:,i) = 0.1*sin(0.03*i);  % control pre-specified
     
@@ -84,25 +91,36 @@ for i = 1:(Ns-1)
     end
     
     % Simulate x1 forward in time
-    interface = u2(:,i) + K*(x1(:,i)-P*x2(:,i));
-    x1_dot = A1*x1(:,i)+B1*interface;
-    x1(:,i+1) = x1(:,i) + x1_dot*dt;
+    interface = u2(:,i) + K*(x1_sim(:,i)-P*x2_sim(:,i));
+    x1_dot = A1*x1_sim(:,i)+B1*interface;
+    x1_sim(:,i+1) = x1_sim(:,i) + x1_dot*dt;
     
     % Simulate x2 forward in time
-    x2_dot = A2*x2(:,i)+B2*u2(:,i);
-    x2(:,i+1) = x2(:,i) + x2_dot*dt;
+    x2_dot = A2*x2_sim(:,i)+B2*u2(:,i);
+    x2_sim(:,i+1) = x2_sim(:,i) + x2_dot*dt;
     
 end
 
 % Plot x1 vs x2
 figure;
 hold on;
-plot(x1(1,:))
-plot(x2(1,:))
+plot(x1_sim(1,:))
+plot(x2_sim(1,:))
 legend("x_1","x_2")
 
-% Plot error
+% Plot V(x1,x2) vs alpha_(|g1-g2|)
 figure;
 hold on;
-error = x1(1,:)-x2(1,:);
+
+V_sim = zeros(1,Ns);
+alpha_sim = zeros(1,Ns);
+for i = 1:Ns
+    V_sim(:,i)= replace(V,[vc;x1;x2;x3],[value(vc);x1_sim(:,i);x2_sim(:,i)]);
+    alpha_sim(:,i) = replace(alpha_,[a_c;x1;x2;x3],[value(a_c);x1_sim(:,i);x2_sim(:,i)]);
+end
+
+plot(V_sim)
+plot(alpha_sim)
+error = x1_sim(1,:)-x2_sim(1,:);
 plot(error)
+legend("V","alpha_","error")
