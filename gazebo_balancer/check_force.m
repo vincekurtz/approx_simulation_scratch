ea3ac407e0194a1b6f68a3584b80d84640aa94ef
Vince Kurtz <vjkurtz@gmail.com>
2019-05-10 08:19:51 -0400

    yalmip and CVX versions

--- gazebo_balancer/check_force.m
+++ gazebo_balancer/check_force.m
@@ -3,7 +3,26 @@ function [res,f_c1,f_c2] = check_force(f_com, com_Xf_c1, com_Xf_c2)
     % is valid, returning 1 if yes and otherwise 0. Depends on the current
     % spatial force transforms to the center of mass 
 
+    tic
     mu = 0.3;
+
+    % YALMIP formulation
+    %f_c1 = sdpvar(2,1);
+    %f_c2 = sdpvar(2,1);
+
+    %F = [ [0;0;0;f_com;0] == com_Xf_c1*[0;0;0;f_c1;0] + com_Xf_c2*[0;0;0;f_c2;0] ];
+    %F = F + [ f_c1(1) <= mu*f_c1(2) ];
+    %F = F + [ f_c2(1) <= mu*f_c2(2) ];
+    %F = F + [ f_c1(1) >= -mu*f_c1(2) ];
+    %F = F + [ f_c2(1) >= -mu*f_c2(2) ];
+    %
+    %res = optimize(F,[]);
+    %res = 1-res.problem;
+
+    %f_c1 = value(f_c1)
+    %f_c2 = value(f_c2)
+
+    % CVX formulation
     cvx_begin quiet
         variable f_c1(2)  % x and y forces on contacts
         variable f_c2(2)
@@ -14,8 +33,13 @@ function [res,f_c1,f_c2] = check_force(f_com, com_Xf_c1, com_Xf_c2)
             [0;0;0;f_com;0] == com_Xf_c1*[0;0;0;f_c1;0] + com_Xf_c2*[0;0;0;f_c2;0]
 
             % forces must obey friction
-            norm(f_c1(1)) <= mu*f_c1(2)
-            norm(f_c2(1)) <= mu*f_c2(2)
+            
+            f_c1(1) <= mu*f_c1(2)
+            f_c2(1) <= mu*f_c2(2)
+            f_c1(1) >= -mu*f_c1(2)
+            f_c2(1) >= -mu*f_c2(2)
+            %norm(f_c1(1)) <= mu*f_c1(2)
+            %norm(f_c2(1)) <= mu*f_c2(2)
     cvx_end
 
     if cvx_status=="Solved"
@@ -23,4 +47,6 @@ function [res,f_c1,f_c2] = check_force(f_com, com_Xf_c1, com_Xf_c2)
     else
         res = 0;
     end
+
+    toc
 end
