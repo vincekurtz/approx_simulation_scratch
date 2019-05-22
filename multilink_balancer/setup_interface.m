99d73a2715d84bc89b3296ac55e4fe381d41eb7d
Vince Kurtz <vjkurtz@gmail.com>
2019-05-22 10:27:04 -0400

    balance based on tracking the LIP model

---
+++ multilink_balancer/setup_interface.m
@@ -0,0 +1,98 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Define parameters of the LIP model and design an associated 
+% interface.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% LIP model
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+h = 1.5;  % height of lip model
+g = 9.81; % acceleration due to gravity
+omega = sqrt(g/h);
+A_lip = [0 1; omega^2 0];
+B_lip = [0; -omega^2];
+
+% Stabilizing controller for LIP model
+Q_lip = diag([1,1]);
+R_lip = 10.0;
+K_lip = lqr(A_lip, B_lip, Q_lip, R_lip);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Interface Design
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Concrete model is a double integrator (from task-space
+% linearization of the CoM dynamics of the balancer).
+A1 = [0 1 0 0;
+      0 0 0 0;
+      0 0 0 1;
+      0 0 0 0];
+B1 = [0 0;
+      1 0;
+      0 0;
+      0 1];
+C1 = eye(4);  % outputs of both systems are the full states
+
+% Abstract model is the LIP, including (dummy) y position and velocity
+A2 = [0 1 0 0;
+      omega^2 0 0 0;
+      0       0 0 1;
+      0       0 0 0];
+B2 = [0        ;
+      -omega^2 ;
+      0        ;
+      0        ];
+C2 = eye(4);
+
+% Symbolic variables for interface design
+u = sdpvar(1);   % external control is position of CoP of the LIP
+x1 = sdpvar(4,1);
+x2 = sdpvar(4,1);
+
+% Interface Derivation
+P = eye(4);  % P and Q chosen such that PA2 = A1P+B1*Q and C2 = C1*P
+Q = [omega^2 0 0 0;
+     0       0 0 0];
+R = [-omega^2;0];
+K_joint = -lqr(A1,B1,eye(4),eye(2));    % A control gain that stabilizes the concrete system
+                                        % Could derive from a PD controller as well
+
+lambda = 0.1;
+Mbar = sdpvar(4,4);
+Kbar = K_joint*Mbar;
+
+F = [ [Mbar Mbar*C1'; C1*Mbar eye(4)] >= 0];
+F = F + [Mbar*A1' + A1*Mbar + Kbar'*B1'+B1*Kbar + 2*lambda*Mbar <= 0 ];
+optimize(F);
+
+M = inv(value(Mbar));
+
+% Double check the original conditions
+check1 = all(eig(M-C1'*C1) >= 0);   % M >= C'C
+check2 = all(eig((A1+B1*K_joint)'*M + M*(A1+B1*K_joint) + 2*lambda*M) <= 0);  % (A+BK)'M+M(A+BK) <= -2lambdaM
+
+if check1 & check2
+    disp("Checks passed!")
+else
+    disp("Checks Failed!")
+    return;
+end
+
+% Now we have an interface given by 
+% u_com = R*u + Q*x2 + K_joint*(x1-P*x2);
+
+% Simulation function
+V = (P*x2-x1)'*M*(P*x2-x1);
+disp("V = (x1-x2)'*M*(x1-x2)")
+disp("M: ")
+M
+disp("")
+
+% Class K function of control input
+gmma = norm(sqrt(M)*(B1*R-P*B2))/lambda;
+disp("Gamma: ")
+sdisplay(gmma)
+disp("")
--- multilink_balancer/spatial_v2_simulation.m
+++ multilink_balancer/spatial_v2_simulation.m
@@ -1,7 +1,8 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 % Use the spatial_v2 library and the associated visualization
-% tools to simulate the multilink balancer. 
+% tools to simulate balencing the multilink balancer via
+% planning with the LIP model.
 %
 % Assumes that create_multilink_spatial_model has already been
 % run, generating a saved model and functions representing the
@@ -11,48 +12,74 @@
 
 clear all; close all; clc;
 
-% Load the model
+% Load the spatial_v2 model of the balancer
 load('balancer_model')
+
+% Derive LIP and linearized CoM dynamics along with an interface
+setup_interface
 
 % Simulation parameters
 T = 10;     % simulation time
 dt = 1e-2;  % timestep
 
-q0 = [0.2;0.2;0.2;0.4];
+q0 = [0.2;0.2;0.2;0.4];   % initial state of full model
 qd0 = zeros(4,1);
-x = [q0;qd0];   % initial state
+x = [q0;qd0];
 
-% Simulation
+% initial state of LIP model
+com_pos = p_com(q0);
+com_vel = pd_com(q0,qd0);
+x_lip = [com_pos(1);com_vel(1);h;0];
+
+% Quantities to save for plotting
 state_trajectory = [];
 com_trajectory = [];
+lip_trajectory = [];
+lip_control = [];
 
 for t = 1:dt:T
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % feedback controller
     tic
     q = x(1:model.NB);    % unpack joint state
     qd = x(model.NB+1:end);  
 
-    x_lin = [p_com(q);J(q)*qd];
-    x_des = [0;1.5;0;0];
+    % Compute LIP controller that will let us balance
+    u_lip = -K_lip*x_lip(1:2);
 
-    K = [1 0 1.7 0; 0 1 0 1.7];
-    v = -K*(x_lin-x_des);       % virtual control for linearized system
+    % Compute the state of the linearized system (CoM x and y position and velocity)
+    com_pos = p_com(q);
+    com_vel = pd_com(q,qd);
+    x_com = [com_pos(1);com_vel(1);com_pos(2);com_vel(2)];
+
+    % Compute virtual control for the feedback linearized system
+    u_com = R*u_lip + Q*x_lip + K_joint*(x_com-P*x_lip);
 
     % Feedback linearization to generate torques as actual control
-    u = J(q)'*(Lambda(q)*v - Lambda(q)*Jdot(q,qd)*qd + Lambda(q)*J(q)*inv(H(q))*C(q,qd));
-    toc
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    u = J(q)'*(Lambda(q)*u_com - Lambda(q)*Jdot(q,qd)*qd + Lambda(q)*J(q)*inv(H(q))*C(q,qd));
 
+    % Simulate the full model forward
     xdot = BalancerDynamics(x,u);
     x = x + xdot*dt;
 
+    % Simulate the lip model forward
+    xdot_lip = A2*x_lip + B2*u_lip;
+    x_lip = x_lip + xdot_lip*dt;
+
     state_trajectory(:,end+1) = x;
     com_trajectory(:,end+1) = p_com(q);
+    lip_trajectory(:,end+1) = x_lip;
+    lip_control(:,end+1) = u_lip;
+    
+    toc
 end
 
-% Plot an animation of the motion
+% Plot an animation of the balancer
 q_trajectory = state_trajectory(1:model.NB,:);
 showmotion(model, 1:dt:T, q_trajectory);
 
+% Plot the CoM trajectory
 plot(1:dt:T,com_trajectory)
+
+% Show an animation of the LIP
+addpath("../balancer")
+figure;
+animate_lip(1:dt:T, lip_trajectory', lip_control',h)
