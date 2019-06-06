767f55c2408d09b5988fe4d8954d902c0da73b35
Vince Kurtz <vjkurtz@gmail.com>
2019-06-06 12:47:06 -0400

    balance based on centroid linearization

--- multilink_balancer/spatial_v2_simulation.m
+++ multilink_balancer/spatial_v2_simulation.m
@@ -15,71 +15,52 @@ clear all; close all; clc;
 % Load the spatial_v2 model of the balancer
 load('balancer_model')
 
-% Derive LIP and linearized CoM dynamics along with an interface
-setup_interface
+% Linearized Centroid Dynamics
+m = model.NB;  % total mass
+A_lin = [zeros(2,3) 1/m*eye(2); zeros(3,5)];
+B_lin = [zeros(2,3);eye(3)];
+K = lqr(A_lin,B_lin,diag([10;10;1;1;1]),0.1*eye(3))
 
 % Simulation parameters
 T = 10;     % simulation time
-dt = 1e-3;  % timestep
+dt = 1e-2;  % timestep
 
-q0 = [0.9;0.2;0.2;0.4];   % initial state of full model
-qd0 = zeros(4,1);
+q0 = [0.2;0.2;0.2;0.4];   % initial state of full model
+qd0 = [0.1;0;0;0];
 x = [q0;qd0];
-
-% initial state of LIP model
-com_pos = p_com(q0);
-com_vel = pd_com(q0,qd0);
-x_lip = [com_pos(1);com_vel(1);h;0];
 
 % Quantities to save for plotting
 state_trajectory = [];
 com_trajectory = [];
-lip_trajectory = [];
-lip_control = [];
 
 for t = 1:dt:T
     tic
     q = x(1:model.NB);    % unpack joint state
     qd = x(model.NB+1:end);  
 
-    % Compute LIP controller that will let us balance
-    u_lip = -K_lip*x_lip(1:2);
+    % Linearization
+    A = [zeros(3,2) eye(3) zeros(3,1)]*A_com(q);    % select only nonzero terms
+    Ad_qd = [zeros(3,2) eye(3) zeros(3,1)]*Ad_com_qd(q,qd);
 
-    % Compute the state of the linearized system (CoM x and y position and velocity)
+    % Compute the state of the linearized system
     com_pos = p_com(q);
-    com_vel = pd_com(q,qd);
-    x_com = [com_pos(1);com_vel(1);com_pos(2);com_vel(2)];
+    com_h = A*qd;
+    x_lin = [com_pos;com_h];
 
-    % Compute virtual control for the feedback linearized system
-    u_com = R*u_lip + Q*x_lip + K_joint*(x_com-P*x_lip);
+    % Compute a control to balance the feedback linearized system
+    x_des = [0;1.5;0;0;0];
+    u_com = -K*(x_lin-x_des);
 
     % Feedback linearization to generate torques as actual control
-    u = J(q)'*(Lambda(q)*u_com - Lambda(q)*Jdot(q,qd)*qd + Lambda(q)*J(q)*inv(H(q))*C(q,qd));
-
-    % Compute null-space projector
-    Jbar = inv(H(q))*J(q)'*Lambda(q);
-    N = (eye(4) - J(q)'*Jbar')';
-
-    % Apply secondary control
-    kp = diag([0;0;0;1]);
-    kd = diag([1;1;1;0]);
-    q_des = [pi/2-0.1;0;0;0.5*sin(t);];
-    qd_des = [0;0;0;0];
-    u0 = -kp*(q-q_des) - kd*(qd-qd_des);  % secondary priority torques
-    u = u + N'*u0;
+    Lambda = inv(A*inv(H(q))*A');
+    u = A'*(Lambda*u_com - Lambda*Ad_qd + Lambda*A*inv(H(q))*C(q,qd));
 
     % Simulate the full model forward
     xdot = BalancerDynamics(x,u);
     x = x + xdot*dt;
 
-    % Simulate the lip model forward
-    xdot_lip = A2*x_lip + B2*u_lip;
-    x_lip = x_lip + xdot_lip*dt;
-
     state_trajectory(:,end+1) = x;
-    com_trajectory(:,end+1) = p_com(q);
-    lip_trajectory(:,end+1) = x_lip;
-    lip_control(:,end+1) = u_lip;
+    com_trajectory(:,end+1) = x_lin;
     
     toc
 end
@@ -88,46 +69,7 @@ end
 q_trajectory = state_trajectory(1:model.NB,:);
 showmotion(model, 1:dt:T, q_trajectory);
 
-% Plot the angular momentum
-true_h_trajectory = [];
-our_h_trajectory = [];
-for t=1:length(state_trajectory)
-    q = state_trajectory(1:4,t);
-    qd = state_trajectory(5:8,t);
-    true_h_trajectory(:,end+1) = Xf_0(q)*h_o(q,qd);  % compute directly
-    our_h_trajectory(:,end+1) = A_com(q)*qd;
-end
-figure;
-subplot(3,1,1)
-plot(1:dt:T,true_h_trajectory)
-subplot(3,1,2)
-plot(1:dt:T,our_h_trajectory)
-subplot(3,1,3)
-plot(1:dt:T,true_h_trajectory-our_h_trajectory)
-
-% plot hdot
-true_hd_trajectory = [];
-our_hd_trajectory = [];
-for t=2:length(state_trajectory)
-    q = state_trajectory(1:4,t);
-    qd = state_trajectory(5:8,t);
-    qdd = (qd-state_trajectory(5:8,t-1))/dt;
-
-    true_hd_trajectory(:,end+1) = (true_h_trajectory(:,t) - true_h_trajectory(:,t-1))/dt;
-    mg = [0;0;0;0;4*g;0];
-    our_hd_trajectory(:,end+1) = A_com(q)*qdd + Ad_com_qd(q,qd);
-end
-figure;
-subplot(3,1,1)
-plot(true_hd_trajectory')
-subplot(3,1,2)
-plot(our_hd_trajectory')
-subplot(3,1,3)
-plot(true_hd_trajectory'-our_hd_trajectory')
-
-
-% Plot the CoM trajectory
-%plot(1:dt:T,com_trajectory)
+plot(com_trajectory');
 
 % Show an animation of the LIP
 addpath("../balancer")
