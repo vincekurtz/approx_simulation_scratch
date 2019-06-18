140465f4760c80c9fa03ed4a19e5b686adef9105
Vince Kurtz <vjkurtz@gmail.com>
2019-06-18 12:36:09 -0400

    formulated MPC as a proper QP

--- multilink_balancer/GenerateLIPTrajectory.m
+++ multilink_balancer/GenerateLIPTrajectory.m
@@ -21,65 +21,30 @@ function u_lip = GenerateLIPTrajectory(x_lip_init, x_com_init, params)
     % Optimization via Direct Collocation %
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-    opti = casadi.Opti();
-    
-    u_lip = opti.variable(1,params.N-1);
-    x_lip = opti.variable(5,params.N);
-    u_com = opti.variable(3,params.N-1);
-    x_com = opti.variable(5,params.N);
+    import casadi.*;
+   
+    % Optimization variables
+    x_com = SX.sym('x_com',5,params.N);
+    u_com = SX.sym('u_com',3,params.N-1);
+    x_lip = SX.sym('x_lip',5,params.N);
+    u_lip = SX.sym('u_lip',1,params.N-1);
+    qp.x = vertcat( x_com(:), u_com(:), x_lip(:), u_lip(:) );
 
     % Cost Function
     QQ = kron(eye(params.N-1),Q);  % matrix with Q on diagonal
     QQ = blkdiag(QQ,Qf);           % the last element is the terminal cost
     RR = kron(eye(params.N-1),R);  % matrix with R on diagonal
-    J = x_lip(:)'*QQ*x_lip(:) + u_lip(:)'*RR*u_lip(:);
+    qp.f = x_lip(:)'*QQ*x_lip(:) + u_lip(:)'*RR*u_lip(:);
 
-    % Constraints
-    opti.subject_to(x_lip(:,1) == x_lip_init);
-    opti.subject_to(x_com(:,1) == x_com_init);
+    % Initial Condition Constraints
+    A_init_com = [eye(5) zeros(5, 5*(params.N-1) + 3*(params.N-1) + 5*params.N + 1*(params.N-1))];
+    A_init_lip = [zeros(5, 5*params.N + 3*(params.N-1)) eye(5) zeros(5, 5*(params.N-1) + 1*(params.N-1))];
+    A_init = [A_init_com;
+              A_init_lip];
+    b_init = [x_com_init;
+              x_lip_init];
 
-    % Constraints on the contact wrench (note that we only consider motion in the x-y plane)
-    mu = 0.2;
-    l = 0.5;
-    mg = -39.24;
-    A = [0 0  0  0  -1 0;   % positive normal force
-         0 0  0  1 -mu 0;   % Coulomb friction
-         0 0  0 -1 -mu 0;
-         0 0  1  0  -l 0;   % Center of pressure constraint
-         0 0 -1  0  -l 0];
-   
-    % Interface Constraint
-    A_interface_eq = [kron(eye(params.N-1), params.R)           ...
-                      kron(eye(params.N-1), params.Q-params.K)  ...
-                      kron(eye(params.N-1), -eye(3))            ...
-                      kron(eye(params.N-1), params.K)];
-    opti.subject_to( A_interface_eq*[u_lip(:); x_lip(1:5*(params.N-1))'; u_com(:); x_com(1:5*(params.N-1))'] == 0 );
-
-    % Acceleration bound (for linearizing contact constraints)
-    bnd = 3.0;
-    opti.subject_to( -bnd <= u_com(2,:) <= bnd );  % bound linear acceleration
-    opti.subject_to( -bnd <= u_com(3,:) <= bnd );
-
-    % Contact constraints
-    Au = A*[zeros(2,3);eye(3);zeros(1,3)];
-    Ax_1 = A*[S([0;mg;0])-S([bnd;bnd;0]);zeros(3)]*[eye(2) zeros(2,3); zeros(1,5)];
-    Ax_2 = A*[S([0;mg;0])-S([-bnd;bnd;0]);zeros(3)]*[eye(2) zeros(2,3); zeros(1,5)];
-    Ax_3 = A*[S([0;mg;0])-S([bnd;-bnd;0]);zeros(3)]*[eye(2) zeros(2,3); zeros(1,5)];
-    Ax_4 = A*[S([0;mg;0])-S([-bnd;-bnd;0]);zeros(3)]*[eye(2) zeros(2,3); zeros(1,5)];
-    b = A*[0;0;0;0;mg;0];
-
-    A_contact_1 = [kron(eye(params.N-1),Au) kron(eye(params.N-1),Ax_1)];
-    A_contact_2 = [kron(eye(params.N-1),Au) kron(eye(params.N-1),Ax_2)];
-    A_contact_3 = [kron(eye(params.N-1),Au) kron(eye(params.N-1),Ax_3)];
-    A_contact_4 = [kron(eye(params.N-1),Au) kron(eye(params.N-1),Ax_4)];
-    b_contact = repmat(b,params.N-1,1);
-
-    opti.subject_to( A_contact_1*[u_com(:);x_com(1:5*(params.N-1))'] <= b_contact )
-    opti.subject_to( A_contact_2*[u_com(:);x_com(1:5*(params.N-1))'] <= b_contact )
-    opti.subject_to( A_contact_3*[u_com(:);x_com(1:5*(params.N-1))'] <= b_contact )
-    opti.subject_to( A_contact_4*[u_com(:);x_com(1:5*(params.N-1))'] <= b_contact )
-
-    % Dynamic (forward Euler) constraints
+    % Dynamic (forward Euler) Constraints
     A_lip_eq = [eye(5)+params.A_lip*params.dt -eye(5) zeros(5,5*(params.N-2))];
     B_lip_eq = [params.B_lip*params.dt zeros(5,params.N-2)];
 
@@ -103,17 +68,93 @@ function u_lip = GenerateLIPTrajectory(x_lip_init, x_com_init, params)
                     circshift(B_com_last_row,[0,3])];
     end
 
-    opti.subject_to( [A_lip_eq B_lip_eq]*[x_lip(:);u_lip(:)] == 0 )
-    opti.subject_to( [A_com_eq B_com_eq]*[x_com(:);u_com(:)] == 0 )
+    A_lip_dynamics = [A_lip_eq B_lip_eq];
+    A_com_dynamics = [A_com_eq B_com_eq];
+    A_dynamics = [A_com_dynamics                  zeros(5*(params.N-1), 5*params.N+1*(params.N-1));
+                  zeros(5*(params.N-1), 5*params.N+3*(params.N-1))                  A_lip_dynamics];
+    b_dynamics = zeros(5*(params.N-1)+5*(params.N-1),1);
 
-    % Run the optimization
-    opti.minimize(J);
+    % Interface Constraints
+    A_interface = [kron(eye(params.N-1), params.K)           ...
+                   zeros(3*(params.N-1),5)                   ...
+                   kron(eye(params.N-1), -eye(3))            ...
+                   kron(eye(params.N-1), params.Q-params.K)  ...
+                   zeros(3*(params.N-1),5)                   ...
+                   kron(eye(params.N-1), params.R)];
+    b_interface = zeros(3*(params.N-1),1);
 
-    options.ipopt.print_level = 0;
-    options.print_time = false;
-    opti.solver('ipopt', options);
-    sol = opti.solve();
-    u_lip = sol.value(u_lip);
+    % Acceleration Bound Constraints
+    bnd = 3.0;
+    A_ucom_bound = [ [eye(2);-eye(2)] zeros(4,1) ];
+    A_ucom_bound = [kron(eye(params.N-1), A_ucom_bound) ];
+    A_bnd = [ zeros(4*(params.N-1), 5*params.N), A_ucom_bound, zeros(4*(params.N-1), 5*(params.N) + 1*(params.N-1))];
+    b_bnd = bnd*ones(4*(params.N-1),1);
+
+    % Contact Constraints
+    mu = 0.2;
+    l = 0.5;
+    mg = -39.24;
+    A = [0 0  0  0  -1 0;   % positive normal force
+         0 0  0  1 -mu 0;   % Coulomb friction
+         0 0  0 -1 -mu 0;
+         0 0  1  0  -l 0;   % Center of pressure constraint
+         0 0 -1  0  -l 0];
+    
+    Au = A*[zeros(2,3);eye(3);zeros(1,3)];
+    Ax_1 = A*[S([0;mg;0])-S([bnd;bnd;0]);zeros(3)]*[eye(2) zeros(2,3); zeros(1,5)];
+    Ax_2 = A*[S([0;mg;0])-S([-bnd;bnd;0]);zeros(3)]*[eye(2) zeros(2,3); zeros(1,5)];
+    Ax_3 = A*[S([0;mg;0])-S([bnd;-bnd;0]);zeros(3)]*[eye(2) zeros(2,3); zeros(1,5)];
+    Ax_4 = A*[S([0;mg;0])-S([-bnd;-bnd;0]);zeros(3)]*[eye(2) zeros(2,3); zeros(1,5)];
+    b = A*[0;0;0;0;mg;0];
+
+    A_contact_1 = [kron(eye(params.N-1),Ax_1) ...
+                   zeros(5*(params.N-1),5)    ...
+                   kron(eye(params.N-1),Au)   ...
+                   zeros(5*(params.N-1),5*params.N+1*(params.N-1))];
+    A_contact_2 = [kron(eye(params.N-1),Ax_2) ...
+                   zeros(5*(params.N-1),5)    ...
+                   kron(eye(params.N-1),Au)   ...
+                   zeros(5*(params.N-1),5*params.N+1*(params.N-1))];
+    A_contact_3 = [kron(eye(params.N-1),Ax_3) ...
+                   zeros(5*(params.N-1),5)    ...
+                   kron(eye(params.N-1),Au)   ...
+                   zeros(5*(params.N-1),5*params.N+1*(params.N-1))];
+    A_contact_4 = [kron(eye(params.N-1),Ax_4) ...
+                   zeros(5*(params.N-1),5)    ...
+                   kron(eye(params.N-1),Au)   ...
+                   zeros(5*(params.N-1),5*params.N+1*(params.N-1))];
+
+    A_contact = [A_contact_1;
+                 A_contact_2;
+                 A_contact_3;
+                 A_contact_4];
+    b_contact = repmat(b,4*(params.N-1),1);
+
+    % Putting constraints together
+    A_eq = [A_init;     % Equality constraints from dynamics, initial conditions, and the interface
+            A_dynamics;
+            A_interface];
+    b_eq = [b_init;
+            b_dynamics;
+            b_interface];
+
+    A_ineq = [A_bnd;        % Inequality constraints from acceleration bound and contact constraints
+              A_contact];  
+    b_ineq = [b_bnd;
+              b_contact];
+
+    qp.g = [A_eq;A_ineq]*[x_com(:);u_com(:);x_lip(:);u_lip(:)];
+
+
+    lbg = [b_eq;-Inf(size(b_ineq))];
+    ubg = [b_eq;b_ineq];
+
+    % Solve the QP
+    S = qpsol('S','qpoases',qp);
+    %S = nlpsol('S','ipopt',qp);
+    r = S('lbg',lbg,'ubg',ubg);
+    x_opt = full(r.x);
+    u_lip = x_opt(end-params.N+2:end)';
 
 end
 
---
+++ multilink_balancer/GenerateLIPTrajectory_opti.m
@@ -0,0 +1,100 @@
+
+function u_lip = GenerateLIPTrajectory(x_lip_init, x_com_init, params)
+    % Compute a trajectory for the LIP model that respects contact constraints
+    % of the full model.
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Parameterizing the Cost Function %
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+   
+    % Running Cost
+    Q = diag([100;0;0;10;0]);
+    R = 0.01;
+
+    % Terminal cost based on optimal cost-to-go of LQR
+    Qf = [32.1091 0 0  0.1544 0;
+           0      0 0  0      0;
+           0      0 0  0      0;
+           0.1544 0 0  0.0491 0;
+           0      0 0  0      0];
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Optimization via Direct Collocation %
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    opti = casadi.Opti();
+    
+    u_lip = opti.variable(1,params.N-1);
+    x_lip = opti.variable(5,params.N);
+    u_com = opti.variable(3,params.N-1);
+    x_com = opti.variable(5,params.N);
+
+    % Cost Function
+    QQ = kron(eye(params.N-1),Q);  % matrix with Q on diagonal
+    QQ = blkdiag(QQ,Qf);           % the last element is the terminal cost
+    RR = kron(eye(params.N-1),R);  % matrix with R on diagonal
+    J = x_lip(:)'*QQ*x_lip(:) + u_lip(:)'*RR*u_lip(:);
+
+    % Constraints
+    opti.subject_to(x_lip(:,1) == x_lip_init);
+    opti.subject_to(x_com(:,1) == x_com_init);
+
+    % Constraints on the contact wrench (note that we only consider motion in the x-y plane)
+    mu = 0.2;
+    l = 0.5;
+    mg = -39.24;
+    A = [0 0  0  0  -1 0;   % positive normal force
+         0 0  0  1 -mu 0;   % Coulomb friction
+         0 0  0 -1 -mu 0;
+         0 0  1  0  -l 0;   % Center of pressure constraint
+         0 0 -1  0  -l 0];
+
+    for t=1:params.N-1
+        % Interface constraint
+        opti.subject_to( u_com(:,t) == params.R*u_lip(:,t) + params.Q*x_lip(:,t) + params.K*(x_com(:,t)-x_lip(:,t)) );
+
+        % Contact constraint
+      
+        % Attempt to use acceleration bound to linearize contact constraints
+        bnd = 3.0;
+        opti.subject_to( -bnd <= u_com(2,t) <= bnd );  % bound linear acceleration
+        opti.subject_to( -bnd <= u_com(3,t) <= bnd );
+
+        Acwc{t,1} = A*[0;0;u_com(:,t);0] + A*[S([0;mg;0])-S([bnd;bnd;0]);zeros(3)]*[x_com(1:2,t);0];
+        Acwc{t,2} = A*[0;0;u_com(:,t);0] + A*[S([0;mg;0])-S([bnd;-bnd;0]);zeros(3)]*[x_com(1:2,t);0];
+        Acwc{t,3} = A*[0;0;u_com(:,t);0] + A*[S([0;mg;0])-S([-bnd;bnd;0]);zeros(3)]*[x_com(1:2,t);0];
+        Acwc{t,4} = A*[0;0;u_com(:,t);0] + A*[S([0;mg;0])-S([-bnd;-bnd;0]);zeros(3)]*[x_com(1:2,t);0];
+
+        opti.subject_to( Acwc{t,1} <= A*[0;0;0;0;mg;0] );
+        opti.subject_to( Acwc{t,2} <= A*[0;0;0;0;mg;0] );
+        opti.subject_to( Acwc{t,3} <= A*[0;0;0;0;mg;0] );
+        opti.subject_to( Acwc{t,4} <= A*[0;0;0;0;mg;0] );
+
+        %% Original contact constraint
+        %Acwc{t} = A*[0;0;u_com(:,t);0] + A*[S([0;mg;0])-S([u_com(2:3);0]);zeros(3)]*[x_com(1:2,t);0];
+        %opti.subject_to( Acwc{t} <= A*[0;0;0;0;mg;0] );
+
+        % Euler integration constraints
+        opti.subject_to(x_lip(:,t+1) == x_lip(:,t) + (params.A_lip*x_lip(:,t) + params.B_lip*u_lip(:,t))*params.dt);
+        opti.subject_to(x_com(:,t+1) == x_com(:,t) + (params.A_com*x_com(:,t) + params.B_com*u_com(:,t))*params.dt);
+
+    end
+
+    % Run the optimization
+    opti.minimize(J);
+
+    options.ipopt.print_level = 0;
+    options.print_time = false;
+    opti.solver('ipopt', options);
+    opti.set_initial(u_lip,1);
+    sol = opti.solve();
+    u_lip = sol.value(u_lip);
+
+end
+
+function y = S(x)
+    % Skew symmetric cross product matrix
+    y = [0     -x(3)   x(2);
+         x(3)   0     -x(1);
+         -x(2)  x(1)    0  ];
+end
